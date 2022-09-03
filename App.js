0901168341d161851b9059ba15039138af3c5d33
Sourabh Singh <sourabhsingh01998@gmail.com>
2022-09-03 20:07:04 +0530

    App UI Update

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from "expo-font";
+import AppLoading from 'expo-app-loading';
 import Home from './screens/Home';
 import Animedetails from './screens/Animedetails';
 import Episodewatch from './screens/Episodewatch';
@@ -11,9 +13,23 @@ import Search from './screens/Search';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'pop-bold': require("./assets/fonts/Poppins-Bold.ttf"),
+    'pop-medium': require("./assets/fonts/Poppins-Medium.ttf"),
+    'pop-regular': require("./assets/fonts/Poppins-Regular.ttf"),
+    'lob-bold': require("./assets/fonts/LobsterTwo-Bold.ttf"),
+    'lob-regular': require("./assets/fonts/LobsterTwo-Regular.ttf"),
+  })
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
+
   return (
     <>
-    <StatusBar style="auto" />
+    <StatusBar style="light" />
     <View style={styles.container}>                 
       <NavigationContainer>
         <Stack.Navigator>
@@ -30,6 +46,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
 });
---
+++ config/styles.js
@@ -0,0 +1,6 @@
+export default {
+    colors:{
+        accentColor: "#FFAF19",
+
+    }
+}
